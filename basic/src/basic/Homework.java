dba6d4609c04cdd53ab4d6eccf38ba36c313803e
cob7 <cob7@njit.edu>
2020-09-15 01:52:46 -0400

    adding my homework

---
+++ basic/src/basic/Homework.java
@@ -0,0 +1,13 @@
+public class Homework {
+	public static void main(String[] args) {
+
+		String name = "Cameron";
+		System.out.println(name);
+
+		int age = (19);
+		System.out.println(age);
+
+		double number = (14.8);
+		System.out.println(number);
+	}
+}
