0c2086e5946fe8d60656b78f1a3986d2f002be48
cob7 <cob7@njit.edu>
2020-12-15 22:42:12 -0500

    adding the homework Java Loops

--- cameronb/src/work1.java
+++ cameronb/src/work1.java
@@ -1,1 +1,17 @@
-gihhkkhhk
+public class work1 {
+	public static void main(String[] args) {
+
+		int[] intArray = { 1, 3, 4, 6, 8, 10, 14, 21, 27 };
+		System.out.println("The numbers:");
+		for (int i = 0; i < intArray.length; i++) {
+			System.out.print(intArray[i] + " ");
+		}
+
+		System.out.println("\nThe even numbers:");
+		for (int i = 0; i < intArray.length; i++) {
+
+			if (intArray[i] % 2 == 0)
+				System.out.print(intArray[i] + " ");
+		}
+	}
+}
